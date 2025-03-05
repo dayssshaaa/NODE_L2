29b9c4cd52556c7ae3680daa2d4c07c53315e654
dayssshaaa <67da4sh164a@gmail.com>
2025-03-05 13:32:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+console.log(`Name: ${process.env.NAME}`);
+console.log(`Surname: ${process.env.SURNAME}`);
+console.log(`Group: ${process.env.GROUP}`);
+console.log(`Number: ${process.env.NUMBER}`);
+console.log(`Mode: ${process.env.MODE}`);
